1863cced88ac91ae84779301f3f6c0938c842644
DevUsaid <muhammadusaidsaleem@gmail.com>
2025-12-20 19:42:19 +0500

    Add dark mode feature and control rendering using useMemo

--- src/App.jsx
+++ src/App.jsx
@@ -1,125 +1,39 @@
-import React, { useEffect, useState,useRef} from 'react'
+import React, { useState } from 'react'
+import NoteApp from './components/Noteapp';
+import { createContext } from "react";
 
 
+export const ThemeContext = createContext();
 
-// useState
+
 const App = () => {
-  const [title, setTitle] = useState("")
-  const [description, setDescription] = useState("")
-  const [task, setTask] = useState([])
-
-  //useRef 
-  const noteRefs = useRef([])
-
-
   
-  // useEffect
+   const [theme,setTheme] = useState('light')
+   
 
-  // local storage
-  // Load tasks from localStorage on mount
-    useEffect(() => {
-    const storage = localStorage.getItem("copyObject")
-    if (storage) {
-      setTask(JSON.parse(storage))
+   // function dark mode 
+   const toggleFunction =()=>{
+    if(theme==='light'){
+      setTheme('dark')
+    }else{
+      setTheme('light')
     }
-  }, [])
-
-  // Save tasks to localStorage whenever task changes//
-  useEffect(() => {
-    localStorage.setItem("copyObject", JSON.stringify(task))
-  }, [task])
+   }
 
 
-
-  const stopFormload = (e) => {
-    e.preventDefault()
-    setTask([...task, { title, description }])
-    setTitle('')
-    setDescription('')
-  }
-
-  const deletNote = (indx) => {
-    setTask(task.filter((_, i) => i !== indx))
-  }
-
-   return (
-    <div className="flex m-10 gap-56 overflow-hidden">
-
-      {/* FORM */}
-      <form className="flex flex-col gap-3 w-96 ml-36" onSubmit={stopFormload}>
-        <h2 className="font-bold text-5xl text-white pb-3">Add Note</h2>
-
-        <input
-          className="p-4 w-96 outline-none"
-          type="text"
-          placeholder="enter a title"
-          value={title}
-          onChange={(e) => setTitle(e.target.value)}
-        />
-
-        <textarea
-          className="p-4 h-52 outline-none"
-          placeholder="write any description"
-          value={description}
-          onChange={(e) => setDescription(e.target.value)}
-        />
-
-        <button className="bg-slate-400 p-3">Add Note</button>
-      </form>
-
-      {/* NOTES */}
-      <div className="border-l-2 p-4">
-        <div className="flex flex-row flex-wrap gap-4">
-
-          {task.map((elem, indx) => {
-
-            // 🔹 ref init
-            if (!noteRefs.current[indx]) {
-              noteRefs.current[indx] = React.createRef()
-            }
-
-            return (
-              <div
-                key={indx}
-                ref={noteRefs.current[indx]}
-                tabIndex={0}
-                onClick={() => noteRefs.current[indx].current.focus()}
-                style={{
-                  backgroundImage:
-                    "url(https://static.vecteezy.com/system/resources/previews/037/152/677/non_2x/sticky-note-paper-background-free-png.png)"
-                }}
-                className="
-                  flex flex-col w-40 pt-9 pb-4 px-4 rounded-xl
-                  bg-cover text-black cursor-pointer
-
-                  focus:outline-none
-                  focus:ring-4
-                  focus:ring-blue-500
-                  focus:ring-offset-2
-                "
-              >
-                <h2 className="text-xl font-bold">{elem.title}</h2>
-                <p>{elem.description}</p>
-
-                <button
-                  onClick={(e) => {
-                    e.stopPropagation()
-                    deletNote(indx)
-                  }}
-                  className="bg-red-500 px-2 py-1 mt-3 rounded-md text-white"
-                >
-                  Delete
-                </button>
-              </div>
-            )
-          })}
-
+  return (
+    <div>
+      <ThemeContext.Provider value={{theme,toggleFunction}}>
+        
+        <div style={{color: theme === "light" ? "#333" : "#fff",
+         border: theme === "light" ? "1px solid #000" : "1px solid #fff",
+         backgroundColor: theme === "light" ? "#fff" : "#333"}}>
+          
+        <NoteApp />
         </div>
-      </div>
+      </ThemeContext.Provider>
     </div>
   )
 }
 
 export default App
-
-// note app
